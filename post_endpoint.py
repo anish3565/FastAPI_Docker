16092da82ca7964f74931cb7c79254b956946ba4
anish3565 <tripathianish12@gmail.com>
2025-07-08 14:40:20 +0530

    post_endpoint file updated

--- post_endpoint.py
+++ post_endpoint.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from helper import 
+from helper import demo_save_in_db
 
 app=FastAPI()
 
@@ -16,3 +16,10 @@ class Message(BaseModel):
 def create_user(user: User):
     return {"content": f"User Created Successfully", "user": user}
     
+@app.post("/save/", response_model=str)
+def save_data(data: User):
+    try:
+        result=demo_save_in_db(data)
+        return result["status"]
+    except Exception as e:
+        return HTTPException(status_code=500, detail="Error saving data: {e}")
